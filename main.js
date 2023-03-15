b8c260a86f0ac6d42f1267a6f8cfdd8158682f28
Rafaelmeritello <rafaelmeritello@gmail.com>
2023-03-15 22:03:03 +0044

    testando, ignore

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname+'/public'));
 app.use('/img',express.static(path.join(__dirname, 'public/images')));
 app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
 app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
